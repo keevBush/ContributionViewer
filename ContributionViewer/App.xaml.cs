a62e89b9b0f011208ec2ef8a10f09662b634d745
keevbush <keevbush@gmail.com>
2024-05-01 23:41:43 +0200

    Ajoutez des fichiers projet.

---
+++ ContributionViewer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ContributionViewer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ContributionViewer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ContributionViewer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
